2bce3e56d68659fc2f1369a8a4291c2c5d9f10d5
Edric Caranto <edric.caranto@gmail.com>
2016-08-22 00:59:41 +0800

    08220216 0059hrs
    08220216 0059hrs

--- Database/rdc_library.sql
+++ Database/rdc_library.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2016 at 11:21 AM
--- Server version: 5.6.26
--- PHP Version: 5.6.12
+-- Generation Time: Aug 21, 2016 at 06:26 PM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,11 +26,49 @@ SET time_zone = "+00:00";
 -- Table structure for table `directive`
 --
 
-CREATE TABLE IF NOT EXISTS `directive` (
+CREATE TABLE `directive` (
   `id` int(11) NOT NULL,
   `directive_date` date NOT NULL,
-  `directive_type` varchar(32) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `directive_type` varchar(45) NOT NULL,
+  `directive_name` varchar(100) NOT NULL,
+  `directive_file` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `directive`
+--
+
+INSERT INTO `directive` (`id`, `directive_date`, `directive_type`, `directive_name`, `directive_file`) VALUES
+(1, '2012-12-12', 'DirectiveType1', 'Directive1', ''),
+(2, '2012-12-12', 'Pretest', 'Pretest1', ''),
+(3, '2012-12-12', 'Pretest', 'Pretest1', ''),
+(4, '0000-00-00', 'Pretest', 'Pretest1', 'uploads/Pretest1,docx'),
+(5, '2012-11-12', 'acceptance', 'acceptance directive', ''),
+(6, '2012-11-12', 'acceptance', 'acceptance directive', ''),
+(7, '2012-11-12', 'acceptance', 'acceptance directive', ''),
+(8, '2012-11-12', 'acceptance', 'acceptance directive', ''),
+(9, '2012-12-12', 'acceptance', 'acceptance directive', ''),
+(10, '2012-11-12', 'acceptance', 'acceptance directive', ''),
+(11, '2012-11-12', 'acceptance', 'acceptance directive', ''),
+(12, '2012-10-12', 'post qualification', 'post qualification', ''),
+(13, '0000-00-00', 'acceptance2', 'acceptancedir', 'uploads/acceptancedir,docx'),
+(17, '0000-00-00', 'acceptance3', 'acceptance directive3', 'uploads/acceptance directive3.docx'),
+(18, '2012-09-12', 'acceptance3', 'acceptance directive3', ''),
+(19, '2012-09-12', 'Pretest', 'Pretest1', ''),
+(20, '0000-00-00', 'postqual', 'postqual directive', ''),
+(21, '2012-09-12', 'acceptance3', 'acceptance directive', ''),
+(22, '0000-00-00', 'DirectiveType1', 'post qualification', 'uploads/post qualification.docx'),
+(23, '2012-11-12', 'DirectiveType1', 'Directive1', ''),
+(24, '2012-12-12', 'acceptance', 'Directive1', ''),
+(25, '2012-12-12', 'acceptance', 'Directive1', ''),
+(26, '2012-09-12', 'acceptance', 'Pretest1', ''),
+(27, '2012-12-16', 'Pretest', 'Pretest sample document', ''),
+(28, '2012-09-12', 'acceptance', 'Pretest1', ''),
+(29, '2012-09-12', 'acceptance', 'Directive1', ''),
+(30, '2012-09-12', 'DirectiveType1', 'post qualification', ''),
+(31, '2012-12-12', 'acceptance2', 'Pretest1', ''),
+(32, '0000-00-00', 'Acceptance', 'new', 'uploads/new.docx'),
+(33, '0000-00-00', 'Acceptance', 'new1', 'uploads/new1.docx');
 
 -- --------------------------------------------------------
 
@@ -38,23 +76,15 @@ CREATE TABLE IF NOT EXISTS `directive` (
 -- Table structure for table `employee`
 --
 
-CREATE TABLE IF NOT EXISTS `employee` (
+CREATE TABLE `employee` (
   `id` int(11) NOT NULL,
-  `emp_rank` varchar(11) NOT NULL,
-  `emp_type` varchar(11) NOT NULL,
-  `emp_fname` varchar(50) NOT NULL,
-  `emp_lname` varchar(20) NOT NULL,
-  `emp_division` varchar(20) NOT NULL,
+  `emp_rank` varchar(45) NOT NULL,
+  `emp_type` varchar(45) NOT NULL,
+  `emp_fname` varchar(45) NOT NULL,
+  `emp_lname` varchar(45) NOT NULL,
+  `emp_division` varchar(45) NOT NULL,
   `user_id` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `employee`
---
-
-INSERT INTO `employee` (`id`, `emp_rank`, `emp_type`, `emp_fname`, `emp_lname`, `emp_division`, `user_id`) VALUES
-(1, 'Lt. Col', 'Army', 'Edric', 'Caranto', 'Director', 1),
-(2, 'Major', 'Army', 'Jon', 'Caranto', 'QAD', 2);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -62,10 +92,24 @@ INSERT INTO `employee` (`id`, `emp_rank`, `emp_type`, `emp_fname`, `emp_lname`, 
 -- Table structure for table `implementation_plan`
 --
 
-CREATE TABLE IF NOT EXISTS `implementation_plan` (
+CREATE TABLE `implementation_plan` (
   `id` int(11) NOT NULL,
-  `implan_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `implan_date` date NOT NULL,
+  `implan_name` varchar(100) NOT NULL,
+  `implan_file` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `implementation_plan`
+--
+
+INSERT INTO `implementation_plan` (`id`, `implan_date`, `implan_name`, `implan_file`) VALUES
+(1, '2012-12-12', 'implan1', '0'),
+(2, '2012-12-12', 'implan2', ''),
+(3, '2012-12-12', 'implan2', ''),
+(4, '0000-00-00', 'implan2', 'uploads/implan2.docx'),
+(5, '2012-12-12', 'implan3', ''),
+(6, '0000-00-00', 'implan3', 'uploads/implan3.docx');
 
 -- --------------------------------------------------------
 
@@ -73,10 +117,23 @@ CREATE TABLE IF NOT EXISTS `implementation_plan` (
 -- Table structure for table `item_specification`
 --
 
-CREATE TABLE IF NOT EXISTS `item_specification` (
+CREATE TABLE `item_specification` (
   `id` int(11) NOT NULL,
-  `itemspec_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `itemspec_date` date NOT NULL,
+  `itemspec_name` varchar(100) NOT NULL,
+  `itemspec_file` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `item_specification`
+--
+
+INSERT INTO `item_specification` (`id`, `itemspec_date`, `itemspec_name`, `itemspec_file`) VALUES
+(1, '2012-12-12', 'itemspecification1', ''),
+(2, '2012-12-12', 'newItemSpec', ''),
+(3, '2012-12-12', 'itemspec2', ''),
+(4, '0000-00-00', 'itemspec2', 'uploads/itemspec2.docx'),
+(5, '0000-00-00', 'itemspecnew', 'uploads/itemspecnew.docx');
 
 -- --------------------------------------------------------
 
@@ -84,7 +141,7 @@ CREATE TABLE IF NOT EXISTS `item_specification` (
 -- Table structure for table `migration`
 --
 
-CREATE TABLE IF NOT EXISTS `migration` (
+CREATE TABLE `migration` (
   `version` varchar(180) NOT NULL,
   `apply_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -94,14 +151,8 @@ CREATE TABLE IF NOT EXISTS `migration` (
 --
 
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
-('m000000_000000_base', 1470452628),
-('m130524_201442_init', 1470452637),
-('m160806_032128_directive', 1470453715),
-('m160806_045425_create_directive', 1470459370),
-('m160806_045835_create_task_organization', 1470460620),
-('m160806_050752_create_result', 1470461221),
-('m160806_050938_create_implementation_plan', 1470461222),
-('m160806_051550_create_item_specification', 1470461223);
+('m000000_000000_base', 1470902919),
+('m130524_201442_init', 1470902926);
 
 -- --------------------------------------------------------
 
@@ -109,11 +160,22 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 -- Table structure for table `result`
 --
 
-CREATE TABLE IF NOT EXISTS `result` (
+CREATE TABLE `result` (
   `id` int(11) NOT NULL,
   `result_date` date NOT NULL,
-  `result_item` varchar(100) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `result_item` varchar(45) NOT NULL,
+  `result_file` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`id`, `result_date`, `result_item`, `result_file`) VALUES
+(1, '2012-12-12', 'result1', ''),
+(2, '2012-12-12', 'ResultItemnew', ''),
+(3, '0000-00-00', 'ResultItemnew', 'uploads/ResultItemnew.docx'),
+(4, '0000-00-00', 'ResultItemnewest', 'uploads/ResultItemnewest.docx');
 
 -- --------------------------------------------------------
 
@@ -121,10 +183,21 @@ CREATE TABLE IF NOT EXISTS `result` (
 -- Table structure for table `task_organization`
 --
 
-CREATE TABLE IF NOT EXISTS `task_organization` (
+CREATE TABLE `task_organization` (
   `id` int(11) NOT NULL,
-  `task_org_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `taskorg_date` date NOT NULL,
+  `taskorg_name` varchar(100) NOT NULL,
+  `taskorg_file` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `task_organization`
+--
+
+INSERT INTO `task_organization` (`id`, `taskorg_date`, `taskorg_name`, `taskorg_file`) VALUES
+(1, '2012-12-12', 'taskorg1', ''),
+(2, '0000-00-00', 'taskorgnew', 'uploads/taskorgnew.docx'),
+(3, '0000-00-00', 'taskorgnewest', 'uploads/taskorgnewest.docx');
 
 -- --------------------------------------------------------
 
@@ -132,12 +205,12 @@ CREATE TABLE IF NOT EXISTS `task_organization` (
 -- Table structure for table `test_document`
 --
 
-CREATE TABLE IF NOT EXISTS `test_document` (
+CREATE TABLE `test_document` (
   `id` int(11) NOT NULL,
   `test_date` date NOT NULL,
-  `test_type` varchar(20) NOT NULL,
+  `test_type` varchar(45) NOT NULL,
   `test_schedule` date NOT NULL,
-  `test_name` varchar(50) NOT NULL,
+  `test_name` varchar(45) NOT NULL,
   `test_worksheet_id` int(11) NOT NULL,
   `task_organization_id` int(11) NOT NULL,
   `result_id` int(11) NOT NULL,
@@ -146,16 +219,33 @@ CREATE TABLE IF NOT EXISTS `test_document` (
   `directive_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `test_document`
+--
+
+INSERT INTO `test_document` (`id`, `test_date`, `test_type`, `test_schedule`, `test_name`, `test_worksheet_id`, `task_organization_id`, `result_id`, `implementation_plan_id`, `item_specification_id`, `directive_id`) VALUES
+(1, '0000-00-00', 'Acceptance', '0000-00-00', 'New Test', 1, 1, 1, 3, 1, 11);
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `test_worksheet`
 --
 
-CREATE TABLE IF NOT EXISTS `test_worksheet` (
+CREATE TABLE `test_worksheet` (
   `id` int(11) NOT NULL,
-  `work_item` varchar(20) NOT NULL
+  `work_item` varchar(46) NOT NULL,
+  `work_file` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `test_worksheet`
+--
+
+INSERT INTO `test_worksheet` (`id`, `work_item`, `work_file`) VALUES
+(1, 'boots', ''),
+(2, 'TestWorksheetnew', 'uploads/TestWorksheetnew.docx'),
+(3, 'TestWorksheetnewest', 'uploads/TestWorksheetnewest.docx');
 
 -- --------------------------------------------------------
 
@@ -163,7 +253,7 @@ CREATE TABLE IF NOT EXISTS `test_worksheet` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
@@ -173,14 +263,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `status` smallint(6) NOT NULL DEFAULT '10',
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'ebcaranto', 'A0vY8cGeW0POngE8eeV64VEsQonYc_8i', '$2y$13$wHSdyFveRpLaOWX7PAJqqOiIf8h0iRRlkG35ZpBNHVM2fOnRMesUa', NULL, 'edric.caranto@gmail.com', 10, 1470452782, 1470452782);
+(1, 'ebcaranto', 'YGesfi39Sth741fU_xTwT3zd9BnNganm', '$2y$13$s.cnfXXfBwd14h9xR.2oDe7KSMx3JZIVkVEYCm09BqNyNYtQgj3mq', NULL, 'ebcaranto@gmail.com', 10, 1471020871, 1471020871),
+(2, 'newuser', 'LdnqkbIY9RzL6_ZWWH9_djGVicZt7Pul', '$2y$13$p.kcVmaptRgvhr3wc5O./OSA6i2RnSGQvMzKKLLCpdh6fsv4fmYoe', NULL, 'newuser@gmail.com', 10, 1471660708, 1471660708);
 
 --
 -- Indexes for dumped tables
@@ -257,42 +348,47 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `directive`
 --
 ALTER TABLE `directive`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 --
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `implementation_plan`
 --
 ALTER TABLE `implementation_plan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `item_specification`
 --
 ALTER TABLE `item_specification`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `result`
 --
 ALTER TABLE `result`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `task_organization`
 --
 ALTER TABLE `task_organization`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `test_document`
+--
+ALTER TABLE `test_document`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `test_worksheet`
 --
 ALTER TABLE `test_worksheet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
